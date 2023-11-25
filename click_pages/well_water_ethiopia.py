e7cec51d910aa9363200e836e6756fb821b39416
awales0177 <40483017+awales0177@users.noreply.github.com>
2023-11-25 17:45:11 -0500

    Update well_water_ethiopia.py

--- click_pages/well_water_ethiopia.py
+++ click_pages/well_water_ethiopia.py
@@ -1,7 +1,6 @@
 import os
 
 import altair as alt
-import numpy as np
 import pandas as pd
 import streamlit as st
 from streamlit_lottie import st_lottie_spinner
@@ -13,16 +12,14 @@ from PIL import Image
 
 def load_well_ethiopia():
     # LAYING OUT THE TOP SECTION OF THE APP
-    row1_1, row1_2, row1_3 = st.columns((2, 3, 2))
+    row1_1, row1_2 = st.columns((9, 3))
     with row1_1:
         st.title("Well Water Project - Ethiopia")
-
-    with row1_2:
-        st.write(
+    st.title("﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌")
+    st.subheader("Bringing Life through Water")
+    st.write(
             """
         ##
-        About Life Source Church: Bringing Life through Water
-
         Welcome to Life Source Church, where our mission goes beyond the walls of our 
         congregation to reach communities in need around the world. Founded on the principles of love, 
         compassion, and service, Life Source Church is committed to making a meaningful impact on the lives of 
@@ -30,37 +27,28 @@ def load_well_ethiopia():
         """
         )
 
-    with row1_3:
+    with row1_2:
         with open("animations/well_animation.json", "r") as f:
             lottie_data = json.load(f)
         st_lottie(lottie_data, loop=True, key="progress")
         #st.write('animation well')
 
-    # LAYING OUT THE MIDDLE SECTION OF THE APP WITH THE MAPS
-    row2_1, row2_2, row2_3, row2_4 = st.columns((2, 1, 1, 1))
-    row3_1, row3_2 = st.columns((1, 1))
-
-    with row3_1:
-        with st.expander("Ethiopia: A Journey of Hope",expanded=True):
-            st.write(
-                """
-                ##   
+    
+    st.image('imgs/page_imgs/ethiopia_water.jpg',caption='Water well in Ethiopia')
+    with st.expander('Mission', expanded=True):
+        st.write(
+            """
                 In Ethiopia, where arid landscapes and unpredictable 
                 climates contribute to water shortages, Life Source Church has embarked on 
                 a journey of hope. Through our dedicated team and partners on the ground, 
                 we are actively involved in digging wells that not only quench the immediate thirst of 
                 communities but also lay the foundation for healthier, more resilient lives.
-                    """)
-        with st.expander("Join Us in Making a Difference",expanded=True):
-            st.write(
-                """
-                ##   
-                At Life Source Church, we invite you to be a part of this life-changing mission. 
+                """)
+
+    st.write("""At Life Source Church, we invite you to be a part of this life-changing mission. 
                 Whether through financial support, volunteering, or spreading awareness, your involvement 
                 can contribute to the creation of a world where every person has access to clean water and the 
                 opportunity for a brighter future.
-                
                 Join us as we dig deep, bring life, and make a lasting impact on the communities of Ethiopia and Colombia.
+
                     """)
-    with row3_2:
-        st.image('imgs/page_imgs/ethiopia_water.jpg')
