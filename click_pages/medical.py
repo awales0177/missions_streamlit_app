2245969631fbb2022dc553fc117e70a5663cb361
awales0177 <40483017+awales0177@users.noreply.github.com>
2023-11-25 17:44:00 -0500

    Update medical.py

--- click_pages/medical.py
+++ click_pages/medical.py
@@ -1,7 +1,6 @@
 import os
 
 import altair as alt
-import numpy as np
 import pandas as pd
 import streamlit as st
 from streamlit_lottie import st_lottie_spinner
@@ -13,12 +12,10 @@ from PIL import Image
 
 def load_medical():
     # LAYING OUT THE TOP SECTION OF THE APP
-    row1_1, row1_2, row1_3 = st.columns((2, 3, 2))
+    row1_1, row1_2 = st.columns((9, 3))
     with row1_1:
-        st.title("HDPTC")
-
-    with row1_2:
-        st.write(
+        st.title("──── HDPTC ────")
+    st.write(
             """
         ##
         Life Source Church Healthcare Missions in Nigeria is committed to 
@@ -28,34 +25,42 @@ def load_medical():
         """
         )
 
-    with row1_3:
+    with row1_2:
         with open("animations/medical_animation.json", "r") as f:
             lottie_data = json.load(f)
         st_lottie(lottie_data, loop=True, key="progress")
         #st.write('animation well')
 
-    # LAYING OUT THE MIDDLE SECTION OF THE APP WITH THE MAPS
-    row2_1, row2_2, row2_3, row2_4 = st.columns((2, 1, 1, 1))
-    row3_1, row3_2 = st.columns((1, 1))
-
-    with row3_1:
-        with st.expander("Key Objectives: Affordable Healthcare for All",expanded=True):
-            st.write("""
-                1). Accessible Clinics: Establishing and supporting accessible healthcare clinics in impoverished areas, ensuring that medical care is within reach for everyone.
-                     
-                2). Health Education: Empowering communities with health education programs, promoting preventive care, and fostering a culture of well-being.
-                     
-                3). Medical Outreach Programs: Conducting regular medical outreach programs to remote villages, providing essential healthcare services and identifying critical health issues.
-                     
-                4). Collaboration with Local Partners: Collaborating with local healthcare organizations, government agencies, and community leaders to create a sustainable healthcare ecosystem.
+    
+    st.subheader("Key Objectives: Affordable Healthcare for All")
+    st.image('imgs/page_imgs/nigeria_h.jpg',caption='Healthcare professionals providing diagnosis')
+    with st.expander('Mission', expanded=True):
+        st.write("""
+            1). Accessible Clinics: Establishing and supporting accessible healthcare clinics in impoverished areas, ensuring that medical care is within reach for everyone.
+                    
+            2). Health Education: Empowering communities with health education programs, promoting preventive care, and fostering a culture of well-being.
+                    
+            3). Medical Outreach Programs: Conducting regular medical outreach programs to remote villages, providing essential healthcare services and identifying critical health issues.
+                    
+            4). Collaboration with Local Partners: Collaborating with local healthcare organizations, government agencies, and community leaders to create a sustainable healthcare ecosystem.
+                """)
+    row2_1, row2_2 = st.columns((1.25, 1))
+    with row2_1:
+        st.write("""This map of Nigeria shows states at the different risk levels of socio-economic vulnerability 
+                 ranked from 1 to 5, spread across the country. This indicates that most parts of the northwest fall 
+                 under high risk (5) compared to states in south-south falling under low risk and medium-low risk (1 and 2) 
+                 respectively. Using geo-tagged household survey data and geospatial covariates (satellite imagery and related 
+                 derived data products including settlement data), GRID3 Nigeria, through its collaboration with Fraym, 
+                 is modelling risk analysis across the country.
                     """)
-        with st.expander("Join Us in the Healing Journey",expanded=True):
-            st.write("""Life Source Church invites individuals, healthcare professionals, 
+        st.write('Source: https://grid3.org/')
+    with row2_2:
+        st.image('imgs/page_imgs/Fraym_map.png',width=300)
+
+    st.write("""Life Source Church invites individuals, healthcare professionals, 
                      and partners to join us in this healing journey. Through financial support, 
                      volunteering, or spreading awareness, you can play a vital role in bringing affordable 
                      healthcare to impoverished communities in Nigeria. Together, let us embody the spirit of Christ by 
                      healing bodies, nurturing souls, and transforming lives through accessible and compassionate healthcare.
 
                     """)
-    with row3_2:
-        st.image('imgs/page_imgs/nigeria_h.jpg')
